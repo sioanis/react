dc525890fa6251a4b5e00f5af71dfe7b103f2add
sioanis <sueann.ioanis@gmail.com>
2015-12-06 13:38:46 -0800

    React js additions to gulp and package.json.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp        = require('gulp'),
   	gutil       = require('gulp-util'),
-  	sass        = require('gulp-sass');
+  	sass        = require('gulp-sass'),
+  	react 		= require('gulp-react');
+
+gulp.task('react', function () {
+  gulp.src('jsx/index.jsx')
+    .pipe(react())
+    .pipe(gulp.dest('dist'));
+});
 
 gulp.task('sass', function () {
   gulp.src('sass/style.scss')
@@ -10,4 +17,5 @@ gulp.task('sass', function () {
  
 gulp.task('default', function () {
   gulp.watch('sass/*.scss', ['sass'])
+  gulp.watch('jsx/*.jsx', ['react'])
 });
